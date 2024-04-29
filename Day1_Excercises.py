ec0d36429353c7edacdd74285decbfd272549e8f
Jules <165840445+julessaldivar@users.noreply.github.com>
2024-04-29 16:31:01 -0400

    day1 lab

---
+++ Day1_Excercises.py
@@ -0,0 +1,16 @@
+echo_string = input("What's up, how are you doing beautiful?")  # exercise 1, echo string
+print(echo_string)
+hug_integer = int(input("How many hugs have you received today?: "))  # exercise 2, add 1 to int
+print("(>'')> you have now received", hug_integer + 1, "hug(s) :)")
+hug_float = float(input("How many more hugs would you like?: "))  # exercise 3, add float
+print(hug_float + .5, ",an extra half hug for good luck <3")
+float1 = float(input("Enter a number: "))  # exercise 4, addition
+float2 = float(input("Enter another number: "))
+print("This is the sum of all the hugs you will ever receive in your entire life:", float1 + float2)
+float3 = float(input("Enter a number: "))  # exercise 5, multiply
+float4 = float(input("Enter yet another number: "))
+print("This is the product of all the missed hug opportunities you let slip away:", float3 * float4)
+int1 = int(input("Enter a number you fool: "))  # exercise 6, division with int
+int2 = int(input("Again! Enter another number!!!: "))
+print("After some consideration it seems like we found a few more hugs for you. Precisely", int1 / int2, "more")
+print("Peace out")
